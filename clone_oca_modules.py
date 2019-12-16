4f787a78f0eebf46a531ccbbe61758443b0f1cc2
Pascal GOUHIER <pascal.gouhier@copagex.fr>
2019-12-16 01:20:13 +0100

    Initialize modules

--- clone_oca_modules.py
+++ clone_oca_modules.py
@@ -12,12 +12,261 @@ OCA_REPOSITORY_NAMES: list of OCA repository names
 """
 import sys
 import os
-from os import path
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 import argparse
 import subprocess
 
-from .oca_projects import OCA_REPOSITORY_NAMES, url
+ALL = ['OCA_PROJECTS', 'OCA_REPOSITORY_NAMES', 'url']
+
+OCA_PROJECTS = {
+    'accounting': ['account-analytic',
+                   'account-budgeting',
+                   'account-closing',
+                   'account-consolidation',
+                   'account-financial-tools',
+                   'account-financial-reporting',
+                   'account-invoice-reporting',
+                   'account-invoicing',
+                   'account-fiscal-rule',
+                   'operating-unit',
+                   'intrastat',
+                   'mis-builder',
+                   'currency',
+                   'credit-control',
+                   'data-protection',
+                   ],
+    # 'backport': ['OCB',
+    #              ],
+    'Apps Store': ['apps-store'],
+
+    'banking': ['bank-payment',
+                'account-reconcile',
+                'bank-statement-import',
+                'account-payment',
+                ],
+    'community': ['maintainer-tools',
+                  'maintainer-quality-tools',
+                  'runbot-addons',
+                  ],
+    'connector': ['connector',
+                  'connector-ecommerce',
+                  'queue',
+                  ],
+    'connector AccountEdge': ['connector-accountedge'],
+    'connector CMIS': ['connector-cmis'],
+    'connector Infor': ['connector-infor'],
+    'connector Lengow': ['connector-lengow'],
+    'connector LIMS': ['connector-lims'],
+    'connector Magento': ['connector-magento'],
+    'connector Prestashop': ['connector-prestashop'],
+    'connector Sage': ['connector-sage'],
+    'connector Salesforce': ['connector-salesforce'],
+    'connector SPSCommerce': ['connector-spscommerce'],
+    'connector WooCommerce': ['connector-woocommerce'],
+    'crm sales marketing': ['sale-workflow',
+                            'crm',
+                            'partner-contact',
+                            'sale-financial',
+                            'sale-reporting',
+                            'commission',
+                            'event',
+                            'survey',
+                            ],
+    'document': ['knowledge'],
+    'ecommerce': ['e-commerce'],
+    'edi': ['edi'],
+    'field-service': ['field-service'],
+    'financial control': ['margin-analysis'],
+    'Infrastructure': ['infrastructure-dns'],
+    'geospatial': ['geospatial'],
+    'hr': ['timesheet',
+           'hr',
+           'department',
+           ],
+    'connector-odoo2odoo': ['connector-odoo2odoo'],
+    'multi-company': ['multi-company'],
+    'l10n-argentina': ['l10n-argentina'],
+    'l10n-austria': ['l10n-austria'],
+    'l10n-belarus': ['l10n-belarus'],
+    'l10n-belgium': ['l10n-belgium'],
+    'l10n-brazil': ['l10n-brazil'],
+    'l10n-cambodia': ['l10n-cambodia'],
+    'l10n-canada': ['l10n-canada'],
+    'l10n-chile': ['l10n-chile'],
+    'l10n-china': ['l10n-china'],
+    'l10n-colombia': ['l10n-colombia'],
+    'l10n-costa-rica': ['l10n-costa-rica'],
+    'l10n-croatia': ['l10n-croatia'],
+    'l10n-ecuador': ['l10n-ecuador'],
+    'l10n-estonia': ['l10n-estonia'],
+    'l10n-ethiopia': ['l10n-ethiopia'],
+    'l10n-finland': ['l10n-finland'],
+    'l10n-france': ['l10n-france'],
+    'l10n-germany': ['l10n-germany'],
+    'l10n-greece': ['l10n-greece'],
+    'l10n-india': ['l10n-india'],
+    'l10n-indonesia': ['l10n-indonesia'],
+    'l10n-iran': ['l10n-iran'],
+    'l10n-ireland': ['l10n-ireland'],
+    'l10n-italy': ['l10n-italy'],
+    'l10n-japan': ['l10n-japan'],
+    'l10n-luxemburg': ['l10n-luxemburg'],
+    'l10n-macedonia': ['l10n-macedonia'],
+    'l10n-mexico': ['l10n-mexico'],
+    'l10n-morocco': ['l10n-morocco'],
+    'l10n-netherlands': ['l10n-netherlands'],
+    'l10n-norway': ['l10n-norway'],
+    'l10n-peru': ['l10n-peru'],
+    'l10n-poland': ['l10n-poland'],
+    'l10n-portugal': ['l10n-portugal'],
+    'l10n-romania': ['l10n-romania'],
+    'l10n-russia': ['l10n-russia'],
+    'l10n-slovenia': ['l10n-slovenia'],
+    'l10n-spain': ['l10n-spain'],
+    'l10n-switzerland': ['l10n-switzerland'],
+    'l10n-taiwan': ['l10n-taiwan'],
+    'l10n-thailand': ['l10n-thailand'],
+    'l10n-turkey': ['l10n-turkey'],
+    'l10n-united-kingdom': ['l10n-united-kingdom'],
+    'l10n-uruguay': ['l10n-uruguay'],
+    'l10n-usa': ['l10n-usa'],
+    'l10n-venezuela': ['l10n-venezuela'],
+    'l10n-vietnam': ['l10n-vietnam'],
+    'logistics': ['carrier-delivery',
+                  'stock-logistics-barcode',
+                  'stock-logistics-workflow',
+                  'stock-logistics-tracking',
+                  'stock-logistics-warehouse',
+                  'stock-logistics-reporting',
+                  'rma',
+                  'ddmrp',
+                  'wms',
+                  ],
+    'manufacturing': ['manufacture',
+                      'manufacture-reporting',
+                      ],
+    'management system': ['management-system'],
+    'purchase': ['purchase-workflow',
+                 'purchase-reporting',
+                 ],
+    'product': ['product-attribute',
+                'product-kitting',
+                'product-variant',
+                'product-pack',
+                ],
+    'project / services': ['project-reporting',
+                           'project-service',
+                           'project-agile',
+                           'contract',
+                           'program',
+                           'business-requirement',
+                           'connector-redmine',
+                           'connector-jira',
+                           ],
+    'social': ['social'],
+    'storage': ['storage'],
+    'search-engine': ['search-engine'],
+    'tools': ['reporting-engine',
+              'report-print-send',
+              'webkit-tools',
+              'server-tools',
+              'server-auth',
+              'server-env',
+              'server-backend',
+              'server-brand',
+              'server-ux',
+              'community-data-files',
+              'webhook',
+              'interface-github',
+              'iot',
+              'rest-framework',
+              ],
+    'vertical association': ['vertical-association'],
+    'vertical hotel': ['vertical-hotel'],
+    'vertical ISP': ['vertical-isp'],
+    'vertical edition': ['vertical-edition'],
+    'vertical education': ['vertical-education'],
+    'vertical medical': ['vertical-medical'],
+    'vertical NGO': ['vertical-ngo',
+                     # XXX
+                     ],
+    'vertical construction': ['vertical-construction'],
+    'vertical real state': ['vertical-realstate'],
+    'vertical travel': ['vertical-travel'],
+    'web': ['web'],
+    'website': ['website',
+                'website-cms',
+                ],
+}
+
+NOT_ADDONS = {
+    'odoo-community.org',
+    'contribute-md-template',
+    'maintainer-tools',
+    'maintainer-quality-tools',
+    'odoo-sphinx-autodoc',
+    'openupgradelib',
+    'connector-magento-php-extension',
+    'OCB',
+    'OpenUpgrade',
+    'pylint-odoo',
+    'oca-custom',
+    'odoorpc',
+    'oca-decorators',
+    'oca-weblate-deployment',
+    'odoo-sentinel',
+}
+
+
+MAIN_BRANCHES = (
+    '6.1',
+    '7.0',
+    '8.0',
+    '9.0',
+    '10.0',
+    '11.0',
+    '12.0',
+    '13.0',
+)
+
+# def get_repositories():
+#     gh = login()
+#     all_repos = [repo.name for repo in gh.repositories_by('OCA')
+#                  if repo.name not in NOT_ADDONS]
+#     return all_repos
+# 
+# 
+# def get_repositories_and_branches(repos=(), branches=MAIN_BRANCHES):
+#     gh = login()
+#     for repo in gh.repositories_by('OCA'):
+#         if repos and repo.name not in repos:
+#             continue
+#         if repo.name in NOT_ADDONS:
+#             continue
+#         for branch in repo.branches():
+#             if branches and branch.name not in branches:
+#                 continue
+#             yield repo.name, branch.name
+
+
+OCA_REPOSITORY_NAMES = []
+for repos in OCA_PROJECTS.values():
+    OCA_REPOSITORY_NAMES += repos
+
+OCA_REPOSITORY_NAMES.sort()
+
+_OCA_REPOSITORY_NAMES = set(OCA_REPOSITORY_NAMES)
+
+_URL_MAPPINGS = {'git': 'git@github.com:%s/%s.git',
+                 'https': 'https://github.com/%s/%s.git',
+                 }
+
+
+def url(project_name, protocol='git', org_name='OCA'):
+    """get the URL for an OCA project repository"""
+    if project_name not in _OCA_REPOSITORY_NAMES:
+        raise ValueError('Unknown project', project_name)
+    return _URL_MAPPINGS[protocol] % (org_name, project_name)
+
 
 
 def clone(organization_remotes=None,
@@ -74,10 +323,6 @@ def main():
           remove_old_repos=args.remove_old_repos)
 
 if __name__ == '__main__':
-    if __package__ is None:
-        import sys
-        from os import path
-        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
     main()
 
 
