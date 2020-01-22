793cc9860dc7b7da6e23f3c0302d981798132327
Pascal GOUHIER <pascal.gouhier@copagex.fr>
2020-01-22 19:26:53 +0100

    Update clone_oca_modules.py

--- clone_oca_modules.py
+++ clone_oca_modules.py
@@ -280,8 +280,12 @@ def clone(organization_remotes=None,
          ):
     for project in OCA_REPOSITORY_NAMES:
         if project in os.listdir('.'):
-            cmd = ['cd', project, ';', 'git', 'pull', '--quiet', ';', 'cd','..']
+            cmd = ['cd', project]
+            cmd2 = ['git', 'pull', '--quiet']
+            cmd3 = ['cd','..']
             subprocess.call(cmd) 
+            subprocess.call(cmd2)
+            subprocess.call(cmd3)
         else:
             cmd = ['git', 'clone', '--quiet', url(project,protocol),'-b', branche, project]
             try:
